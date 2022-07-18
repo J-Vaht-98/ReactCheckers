13697800b1ae33acc069df02cc998a6b13f1b55f
JVaht-98 <jaagup.vaht@gmail.com>
2022-07-18 23:05:38 +0300

    added comments

--- game/src/App.js
+++ game/src/App.js
@@ -1,10 +1,17 @@
 import Board from './Components/Board'
 import './App.css'
-import {useRef,useContext, createContext, useState} from 'react'
+import {useRef, useState} from 'react'
 import Game from './Game/Game'
-import GameInfo from './Components/GameInfo'
 function App() {
   const [newGame, setNewGame] = useState(false);
+  /**
+   *  Gamestate: 
+   *    0 - empty square
+   *    1 - player 1 button
+   *    2 - player 2 button 
+   *   -1 - player 1 king
+   *   -2 - player 2 king
+   */
   const gameState = [
     [2,0,2,0,2,0,2,0],
     [0,2,0,2,0,2,0,2],
@@ -15,22 +22,18 @@ function App() {
     [1,0,1,0,1,0,1,0],
     [0,1,0,1,0,1,0,1],
   ]
-  const gameState2 = [
+  const gameState2 = [ //this just a dummy gamestate for debugging
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
-    [0,0,-1,0,0,0,0,0],
-    [0,0,0,-2,0,0,0,0],
     [0,0,0,0,0,0,0,0],
-    [0,1,0,0,0,1,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
 
   ]
   const game = useRef(new Game(gameState));
-  const handleNewGame = ()=>{
-      game.current.init()
-      newGame ? setNewGame(false):setNewGame(true)
-  }
   return ( <>
       <Board gameRef={game} newGame={newGame}/>
       
